f71145d6a3cc41128f4c42c4a4e0ef4c61d3f9f2
due2401 <hibik610@gmail.com>
2017-03-04 13:08:02 +0800

    初始化專案

---
+++ CS280-Homework-1.2/CS280-Homework-1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS280_Homework_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
