b58507036b9ff1ccbc7ec58bd9d9ac466c701ff9
due2401 <hibik610@gmail.com>
2017-03-04 22:40:21 +0800

    修改Pokemon類別

---
+++ CS280-Homework-1.2/CS280-Homework-1.2/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS280_Homework_1._2
+{
+    internal class Pokemon
+    {
+        public string Number;
+        public string Name;
+        public float Height;
+        public float Weight;
+        public string Abilities;
+        public string Categroy;
+        public string Gender;
+    }
+}
--- CS280-Homework-1.2/CS280-Homework-1.2/Program.cs
+++ CS280-Homework-1.2/CS280-Homework-1.2/Program.cs
@@ -5,10 +5,25 @@ using System.Text;
 
 namespace CS280_Homework_1._2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Pokemon Pokemon = new Pokemon();
+            Pokemon.Number = "#078";
+            Pokemon.Name = "Rapidash";
+            Pokemon.Height = 5.0f;
+            Pokemon.Weight = 209.4f;
+            Pokemon.Abilities = "Flash Fire";
+            Pokemon.Categroy = "Fire Horse";
+            Pokemon.Gender = "F";
+            Console.WriteLine("編號: {0}", Pokemon.Number);
+            Console.WriteLine("名稱: {0}", Pokemon.Name);
+            Console.WriteLine("身高: {0} 尺", Pokemon.Height);
+            Console.WriteLine("體重: {0} 磅", Pokemon.Weight);
+            Console.WriteLine("技能: {0}", Pokemon.Abilities);
+            Console.WriteLine("種類: {0}", Pokemon.Categroy);
+            Console.WriteLine("性別: {0}", Pokemon.Gender);
         }
     }
 }
